0f2ac0077242f79ca7dc3d12c94aac4a6e7c0c8c
skink001 <vamsikinkiri@gmail.com>
2024-11-06 20:52:45 -0800

    New comment

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
-
+#comment
 namespace Basic_Bank
 {
     public partial class WebForm1 : System.Web.UI.Page
